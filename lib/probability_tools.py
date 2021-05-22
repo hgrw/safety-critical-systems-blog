d7ab90d070de93f9bae28b4c4118d84e153c6743
hgrw <harry.roache-wilson@appliedev.com>
2021-05-22 17:42:18 +1000

    fixed bug in probability tools. And, Or were wrong way around

--- lib/probability_tools.py
+++ lib/probability_tools.py
@@ -4,9 +4,9 @@ from lib.plot_tools import to_precision
 
 def node_probability(operator, probs):
 
-    if operator == 'Or':
+    if operator == 'And':
         return to_precision(1 - np.prod([1 - float(member) for member in probs]), 3)
-    elif operator == 'And':
+    elif operator == 'Or':
         return to_precision(np.prod(np.array(probs).astype(float)), 3)
     else:
         print("Invalid Operator: ", operator)
